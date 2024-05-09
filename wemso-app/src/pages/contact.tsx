45209e77e232f31595fc64f5362d12a47a965eff
hm239 <hm239@students.waikato.ac.nz>
2024-05-09 14:59:21 +1200

    Creating a website in construction page

--- wemso-app/src/pages/contact.tsx
+++ wemso-app/src/pages/contact.tsx
@@ -1,12 +1,37 @@
 // Filename - pages/contact.js
 
 import React from "react";
+import BurgerMenu from "../components/BurgerMenu.tsx";
 import "../App.css";
 
 const Contact: React.FC = () => {
   return (
-    <div>
-      <h1>Website under construction</h1>
+    <div className="App">
+      <div className="background">
+        <div className="navbar">
+          <div className="nav-left">
+            <p id="logo">
+              <a href="/"> 
+                  <img
+                    id="imgLogo"
+                    src={require("..//images/WESMOLogo.png")}
+                    alt="Wesmo logo"
+                />
+                </a>
+            </p>
+          </div>
+          <div className="nav-right">
+            <BurgerMenu />
+            <div className="nav-right"></div>
+          </div>
+        </div>
+        <div id="mainTitle">
+          <h1 className="text-3xl font-bold underline">
+            Website under construction
+          </h1>
+          <h3>More coming soon</h3>
+        </div>
+      </div>
     </div>
   );
 };
--- wemso-app/src/pages/index.tsx
+++ wemso-app/src/pages/index.tsx
@@ -39,7 +39,7 @@ const Home: React.FC = () => {
             </div>
           </a>
         </div>
-      </div>{" "}
+      </div>
     </div>
   );
 };
