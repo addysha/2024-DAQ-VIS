8f4d2dfc59a859674733f6c14da8133fb55b980a
hm239 <hm239@students.waikato.ac.nz>
2024-06-07 22:08:35 +1200

    About wesmo page complete

--- wesmo-app/src/pages/about.tsx
+++ wesmo-app/src/pages/about.tsx
@@ -3,12 +3,20 @@
 import React from "react";
 import BurgerMenu from "../components/BurgerMenu.tsx";
 import Logo from "../components/Logo.tsx";
+import TitleCard from "../components/TitleCard.tsx";
+
+import logo from "../images/logo_header.png";
+
 import "../App.css";
 
 const About: React.FC = () => {
   return (
     <div className="App">
-      <div className="background">
+      <div className="background-about">
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto Condensed"
+          rel="stylesheet"
+        ></link>
         <div className="navbar">
           <div className="nav-left">
             <Logo />
@@ -18,11 +26,32 @@ const About: React.FC = () => {
             <div className="nav-right"></div>
           </div>
         </div>
-        <div id="mainTitle">
-          <h1 className="text-3xl font-bold underline">
-            Website under construction
-          </h1>
-          <h3>More coming soon</h3>
+        <br />
+        <TitleCard title="Waikato Engineering School Motorsport Organisation" />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="image-text-component-about">
+          <div className="text-container-about">
+            WESMO was founding in 2006.
+            <br />
+            <br />
+            The Wesmo team is gearing up for the Formula SAE competition, a
+            competition in which the University of Waikato, under the name
+            WESMO, competes against 30-35 other teams from across Oceania.
+          </div>
+          <div className="image-container-right">
+            <img src={logo} alt="" className="image-right" />
+          </div>
         </div>
       </div>
     </div>
