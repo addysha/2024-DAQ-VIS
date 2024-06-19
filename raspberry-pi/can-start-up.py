eba1dcc5091fff7036d241b6f87193f88b68ca94
hm239 <hm239@students.waikato.ac.nz>
2024-06-19 19:11:35 +1200

    Testing setup for test bench, data collected

--- raspberry-pi/can-start-up.py
+++ raspberry-pi/can-start-up.py
@@ -10,14 +10,18 @@ import can
 
 def create_devices():
     try:
-        os.system("sudo ip link set can0 type can bitrate 100000")
-        os.system("sudo ip link set can1 type can bitrate 100000")
-        os.system("sudo ipconfig can0 up")
-        os.system("sudo ipconfig can1 up")
+        os.system("sudo ip link set can0 type can bitrate 500000")
+        os.system("sudo ip link set can1 type can bitrate 500000")
+        os.system("sudo ifconfig can0 up")
+        os.system("sudo ifconfig can1 up")
         global can0
         global can1
         can0 = can.interface.Bus(channel="can0", bustype="socketcan")
         can1 = can.interface.Bus(channel="can0", bustype="socketcan")
+        # This doesnt return a failure
+        # RTNETLINK answers: Device or resource busy
+        # RTNETLINK answers: Device or resource busy
+
         print("Setup complete for can devices: can0 and can1")
         return True
     except Exception as e:
@@ -38,19 +42,27 @@ def shutdown():
 
 # Main loop
 def main():
+    #shutdown()
     success = create_devices()
 
     if success:
         print("Initilising Loop")
         while True:
             msg0 = can0.recv(0.0)
-            msg1 = can1.recv(0.0)
+            
+            # Live printing
+        #    if msg0 is not None:
+        #        print(msg0)
+        #    else:
+        #        print("No message on can0")
 
-            with open("testing-output.txt", "w") as writer:
+
+            # Store in file
+            with open("testing-output.txt", "a") as writer:
                 if msg0 is not None:
-                    writer.write(msg0)
-                if msg1 is not None:
-                    writer.write(msg1)
+                    writer.write('\n')
+                    writer.write(str(msg0))
+                    
             writer.close()
     else:
         print("Beginning shutdown process")
