1713382008b1bfd5e2feaabb66873efa6f2ab43f
hm239 <hm239@students.waikato.ac.nz>
2024-07-12 20:47:38 +1200

    Completed initial mc translator

--- raspberry-pi/translator.py
+++ raspberry-pi/translator.py
@@ -1,8 +1,6 @@
 import re
-import canmatrix
 import canmatrix.formats
-import pickle
-import MotorControllerClass as mc
+import datetime
 
 
 def load_dbc(dbc_file_path):
@@ -20,95 +18,129 @@ def load_dbc(dbc_file_path):
         return None
 
 
-def get_mc_object(line):
-    id = parametername = objecttype = datatype = accesstype = PDOmapping = subnumber = (
-        defaultvalue
-    ) = lowlimit = highlimit = notes = None
-    data = line.split("\n")
-    for i in range(len(data)):
-        if data[i].startswith("["):
-            id = data[i].strip("[]")
-        elif data[i].startswith("ParameterName"):
-            parametername = data[i].split("=")[1].strip()
-        elif data[i].startswith("ObjectType"):
-            objecttype = data[i].split("=")[1].strip()
-        elif data[i].startswith("DataType"):
-            datatype = data[i].split("=")[1].strip()
-        elif data[i].startswith("AccessType"):
-            accesstype = data[i].split("=")[1].strip()
-        elif data[i].startswith("PDOMapping"):
-            PDOmapping = data[i].split("=")[1].strip()
-        elif data[i].startswith("SubNumber"):
-            subnumber = data[i].split("=")[1].strip()
-        elif data[i].startswith("DefaultValue"):
-            defaultvalue = data[i].split("=")[1].strip()
-        elif data[i].startswith("LowLimit"):
-            lowlimit = data[i].split("=")[1].strip()
-        elif data[i].startswith("HighLimit"):
-            highlimit = data[i].split("=")[1].strip()
-        elif data[i].startswith(";;"):
-            notes = data[i]
-        else:
-            print(f"Error: Invalid parameter in EDS file. {data[i]}")
-    mc_object = mc.MotorController(
-        id,
-        parametername,
-        objecttype,
-        datatype,
-        accesstype,
-        PDOmapping,
-        subnumber,
-        defaultvalue,
-        lowlimit,
-        highlimit,
-        notes,
+def decode_can_string(can_data):
+    can_data = can_data.split()
+    dl = int(can_data[7])
+
+    values = [
+        can_data[1],
+        can_data[3],
+        str(dl),
+        "".join(can_data[8 : 8 + dl]),
+        can_data[8 + dl + 1],
+    ]
+    return values
+
+
+def decode_mc_pdo_4(can_data):
+    if len(can_data) != 16 and len(can_data) != 14:
+        print(f"Invalid PDO 4 message length {can_data}")
+
+    torque_regulator = int(can_data[1] + can_data[0], 16)
+    flux_regulator_count = int(can_data[3] + can_data[2], 16)
+    if len(can_data) == 14:
+        velocity_actual_value = int(can_data[6] + can_data[5] + can_data[4], 16)
+    else:
+        velocity_actual_value = int(
+            can_data[7] + can_data[6] + can_data[5] + can_data[4], 16
+        )
+
+    return [
+        f"torque regulator: {torque_regulator}",
+        f"flux regulator count: {flux_regulator_count}",
+        f"velocity actual value: {velocity_actual_value}",
+    ]
+
+
+def decode_mc_pdo_3(can_data):
+    if len(can_data) != 12 and len(can_data) != 16:
+        print(f"Invalid PDO 3 message length {can_data}")
+
+    motor_current_actual = int(can_data[1] + can_data[0], 16)
+    electrical_angle = int(can_data[3] + can_data[2], 16)
+    phase_a_current = int(can_data[5] + can_data[4], 16)
+
+    data = [
+        f"motor current actual: {motor_current_actual}",
+        f"electrical angle: {electrical_angle}",
+        f"phase a current: {phase_a_current}",
+    ]
+
+    if len(can_data) == 16:
+        phase_b_current = int(can_data[7] + can_data[6], 16)
+        data += [f"phase b current: {phase_b_current}"]
+
+    return data
+
+
+def decode_mc_pdo_2(can_data):
+    if len(can_data) != 16:
+        print(f"Invalid PDO 2 message length {can_data}")
+
+    controller_temp = int(can_data[0], 16)
+    motor_temp = int(can_data[1], 16)
+    DC_link_circuit_voltage = int(can_data[3] + can_data[2], 16)
+    logic_power_supply_voltage = int(can_data[5] + can_data[4], 16)
+    current_demand = int(can_data[7] + can_data[6], 16)
+
+    return [
+        f"controller temp: {controller_temp}",
+        f"motor temp: {motor_temp}",
+        f"DC link circuit voltage: {DC_link_circuit_voltage}",
+        f"logic power supply voltage: {logic_power_supply_voltage}",
+        f"current demand: {current_demand}",
+    ]
+
+
+def decode_mc_pdo_1(can_data):
+    if len(can_data) != 16:
+        print(f"Invalid PDO 1 message length {can_data}")
+
+    status_word = int(can_data[1] + can_data[0], 16)
+    position_actual_value = int(
+        can_data[5] + can_data[4] + can_data[3] + can_data[2], 16
     )
-    return mc_object
+    torque_actual_value = int(can_data[7] + can_data[6], 16)
 
-
-def add_to_db(mc_object):
-    with open("mc_objects.pkl", "ab") as outp:
-        pickle.dump(mc_object, outp, pickle.HIGHEST_PROTOCOL)
+    return [
+        f"status word: {status_word}",
+        f"position actual: {position_actual_value}",
+        f"torque actual: {torque_actual_value}",
+    ]
 
 
 def main():
-    mc_objects = []
-    with open("eds/motor_controller.eds", "r") as eds_file:
-        eds_data = eds_file.read()
-        eds_data = eds_data.split("\n\n")
-        for line in eds_data:
-            if re.match(r"\[\d{3}\w\]", line) or re.match(r"\[\d{4}sub\d\]", line):
-                obj = get_mc_object(line)
-                mc_objects.append(obj)
-        eds_file.close()
+    decoded_can = []
+    with open("data/can_str_test.txt", "r") as can_input_file:
+        can_data = can_input_file.read()
+        can_data = can_data.split("\n")
+        for line in can_data:
+            can_data = decode_can_string(line)
+            data = [f"time: {datetime.datetime.fromtimestamp(float(can_data[0]))}"]
 
-        print(f"Objects found: {len(mc_objects)}")
+            if can_data[1] == "0181":
+                data += decode_mc_pdo_1(can_data[3])
+            elif can_data[1] == "0281":
+                data += decode_mc_pdo_2(can_data[3])
+            elif can_data[1] == "0381":
+                data += decode_mc_pdo_3(can_data[3])
+            elif can_data[1] == "0481":
+                data += decode_mc_pdo_4(can_data[3])
+            else:
+                continue
+            decoded_can.append(data)
 
-        with open("mc_objects.pkl", "wb") as outp:
-            pickle.dump(mc_objects, outp, pickle.HIGHEST_PROTOCOL)
+        can_input_file.close()
 
-        # UNPICKLE THE OBJECT
-        with open("mc_objects.pkl", "rb") as outp:
-            mc_objects = pickle.load(outp)
-            for obj in mc_objects:
-                print(obj)
+    # Eventually load into a db
+    for data in decoded_can:
+        print(data)
 
-    # db = load_dbc("dbc/motor_controller.dbc")
+    # wont work for the motor controller due to the PDO formatting
+    # db = cantools.database.load_file("motor_controller.dbc")
+    # for message in db.messages:
+    #     print(db.decode_message(message.id, message.data))
 
-    # if db:
-
-    # can_data = read_can_file("raspberry-pi/data/can_str_test.txt")
-    # Decode CAN messages using the DBC database
-    # decoded_messages = decode_can_messages(can_data, db)
-
-    # print(decoded_messages)
-    # Print decoded messages
-    # for message in decoded_messages:
-    #     if message["name"] == "Receive_PDO_2_Mapping":
-    #         print(message)
-
-
-data_file = "data/test_csv_data.csv"
 
 if __name__ == "__main__":
     main()
