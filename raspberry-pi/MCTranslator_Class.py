9d829c4a2138eebbc2178ea0d606070f3638b277
hm239 <hm239@students.waikato.ac.nz>
2024-07-14 17:47:47 +1200

    Updating the translation of mc data to a class and setting up the main script for testing

---
+++ raspberry-pi/MCTranslator_Class.py
@@ -0,0 +1,107 @@
+import datetime
+
+
+class MCTranslator:
+    def __init__(self):
+        pass
+
+    def decode_can_string(self, can_data):
+        can_data = can_data.split()
+        dl = int(can_data[7])
+
+        values = [
+            can_data[1],
+            can_data[3],
+            str(dl),
+            "".join(can_data[8 : 8 + dl]),
+            can_data[8 + dl + 1],
+        ]
+        return values
+
+    def decode_mc_pdo_4(self, can_data):
+        if len(can_data) != 16 and len(can_data) != 14:
+            print(f"Invalid PDO 4 message length {can_data}")
+
+        torque_regulator = int(can_data[2:4] + can_data[0:2], 16)
+        flux_regulator_count = int(can_data[6:8] + can_data[4:6], 16)
+        if len(can_data) == 14:
+            velocity_actual_value = int(
+                can_data[12:14] + can_data[10:12] + can_data[8:10], 16
+            )
+        else:
+            velocity_actual_value = int(
+                can_data[16:14] + can_data[12:14] + can_data[10:12] + can_data[8:10], 16
+            )
+
+        return [
+            f"torque regulator: {torque_regulator}",
+            f"flux regulator count: {flux_regulator_count}",
+            f"velocity actual value: {velocity_actual_value}",
+        ]
+
+    def decode_mc_pdo_3(self, can_data):
+        if len(can_data) != 12 and len(can_data) != 16:
+            print(f"Invalid PDO 3 message length {can_data}")
+
+        motor_current_actual = int(can_data[2:4] + can_data[0:2], 16)
+        electrical_angle = int(can_data[6:8] + can_data[4:6], 16)
+        phase_a_current = int(can_data[10:12] + can_data[8:10], 16)
+
+        data = [
+            f"motor current actual: {motor_current_actual}",
+            f"electrical angle: {electrical_angle}",
+            f"phase a current: {phase_a_current}",
+        ]
+
+        if len(can_data) == 16:
+            phase_b_current = int(can_data[16:14] + can_data[12:14], 16)
+            data += [f"phase b current: {phase_b_current}"]
+
+        return data
+
+    def decode_mc_pdo_2(self, can_data):
+        if len(can_data) != 16:
+            print(f"Invalid PDO 2 message length {can_data}")
+
+        controller_temp = int(can_data[0:2], 16)
+        motor_temp = int(can_data[2:4], 16)
+        DC_link_circuit_voltage = int(can_data[6:8] + can_data[4:6], 16)
+        logic_power_supply_voltage = int(can_data[10:12] + can_data[8:10], 16)
+        current_demand = int(can_data[16:14] + can_data[12:14], 16)
+        return [
+            f"controller temp: {controller_temp}",
+            f"motor temp: {motor_temp}",
+            f"DC link circuit voltage: {DC_link_circuit_voltage}",
+            f"logic power supply voltage: {logic_power_supply_voltage}",
+            f"current demand: {current_demand}",
+        ]
+
+    def decode_mc_pdo_1(self, can_data):
+        if len(can_data) != 16:
+            print(f"Invalid PDO 1 message length {can_data}")
+
+        status_word = int(can_data[0:2] + can_data[2:4], 16)
+        position_actual_value = int(
+            can_data[10:12] + can_data[8:10] + can_data[6:8] + can_data[4:6], 16
+        )
+        torque_actual_value = int(can_data[16:14] + can_data[12:14], 16)
+
+        return [
+            f"status word: {status_word}",
+            f"position actual: {position_actual_value}",
+            f"torque actual: {torque_actual_value}",
+        ]
+
+    def decode_pdo(self, can_data, pdo=None):
+        data = [f"time: {datetime.datetime.fromtimestamp(float(can_data[0]))}"]
+
+        if can_data[1] == "0181" and pdo == 1:
+            data += self.decode_mc_pdo_1(can_data[3])
+        elif can_data[1] == "0281" and pdo == 2:
+            data += self.decode_mc_pdo_2(can_data[3])
+        elif can_data[1] == "0381" and pdo == 3:
+            data += self.decode_mc_pdo_3(can_data[3])
+        elif can_data[1] == "0481" and pdo == 4:
+            data += self.decode_mc_pdo_4(can_data[3])
+
+        print(data)
--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -1,8 +1,8 @@
 import csv
 import random
 import argparse
-from datetime import date
 from paho.mqtt import client as mqtt_client
+from MCTranslator_Class import MCTranslator
 
 
 """ GLOBAL VARIABLES
@@ -18,40 +18,8 @@ client_id = f"wesmo-{random.randint(0, 100)}"
 username = "wesmo"
 password = "public"
 
-
-def text_to_csv(input_file, output_file):
-    """_summary_
-    Converts CAN string messages to csv fomatted data.
-    Using input file 'test_data.txt' and output file 'test_csv_data.csv'
-        Args:
-            input_file (str): The txt file containing CAN messages.
-            output_file (str): The file to write the csv formatted data.
-    """
-    csv_data = []
-    headers = ["Timestamp", "ID", "Data Length", "Data Payload", "Channel"]
-    csv_data.append(headers)
-
-    with open(input_file, "r") as file:
-        for line in file:
-            if line.strip():
-                parts = line.strip().split()
-                timestamp = parts[1]
-                id_ = parts[3]
-                data_length_index = parts.index("DL:") + 1
-                data_length = parts[data_length_index]
-                data_payload_index = data_length_index + 1
-                data_payload = " ".join(
-                    parts[data_payload_index : data_payload_index + int(data_length)]
-                )
-                channel = parts[-1]
-
-                csv_data.append([timestamp, id_, data_length, data_payload, channel])
-
-    with open(output_file, "w", newline="") as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerows(csv_data)
-
-    print(f'CSV file "{output_file}" has been created successfully.')
+mc_translator = MCTranslator()
+pdo = 1
 
 
 def connect_mqtt() -> mqtt_client:
@@ -82,14 +50,11 @@ def subscribe(client: mqtt_client):
     """
 
     def on_message(client, userdata, msg):
-        can_msg = msg.payload.decode()
-        print(can_msg)
-        with open(f"data/{date.today()}-data.txt", "a") as writer:
-            if can_msg is not None:
-                writer.write(can_msg)
-                writer.write("\n")
 
-        writer.close()
+        can_msg = msg.payload.decode()
+
+        can_data = mc_translator.decode_can_string(can_msg)
+        mc_translator.decode_pdo(can_data, pdo)
 
     client.subscribe(topic)
     client.on_message = on_message
@@ -99,26 +64,11 @@ def main():
     """Main function to run the script as a reciever device.
 
     The script can be run with the following command line arguments:
-    -c, --convert: Convert the input file to CSV format.
-    -i, --input_file: The file containing the CAN messages.
-    -o, --output_file: The file to write the CSV formatted data.
     -s, --subscribe: Subscribe to CAN messages using MQTT.
     """
 
     parser = argparse.ArgumentParser(
-        description="Converts CAN string messages to CSV formatted data."
-    )
-    parser.add_argument(
-        "-c",
-        "--convert",
-        action="store_true",
-        help="Convert the input file to CSV format",
-    )
-    parser.add_argument(
-        "-i", "--input_file", help="The file containing the CAN messages"
-    )
-    parser.add_argument(
-        "-o", "--output_file", help="The file to write the CSV formatted data"
+        description="Converts CAN string messages to human readable data."
     )
     parser.add_argument(
         "-s",
@@ -129,24 +79,12 @@ def main():
 
     args = parser.parse_args()
 
-    if args.convert and args.subscribe:
-        print(
-            "Error: Cannot use both -c/--convert and -s/--subscribe options at the same time."
-        )
-        exit(1)
-    elif args.convert:
-        if not args.input_file or not args.output_file:
-            print(
-                "Error: Please provide both input_file and output_file when using -c/--convert option."
-            )
-            exit(1)
-        text_to_csv(args.input_file, args.output_file)
-    elif args.subscribe:
+    if args.subscribe:
         client = connect_mqtt()
         subscribe(client)
         client.loop_forever()
     else:
-        print("Error: Please specify either -c/--convert or -s/--subscribe option.")
+        print("Error: Please specify either  -s/--subscribe option.")
         exit(1)
 
 
