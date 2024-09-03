40e4ca910aff66afe44714e781d87149cd0d9bd4
hm239 <hm239@students.waikato.ac.nz>
2024-09-03 12:24:07 +1200

    Updating dummy data to fit new historic data format

--- wesmo-app/src/App.py
+++ wesmo-app/src/App.py
@@ -1,61 +1,40 @@
 from flask import Flask, request
 from flask_socketio import SocketIO
 import time
+import datetime
 from dummy_data import DummySensorData
-
 
 app = Flask(__name__)
 socketio = SocketIO(app, logger=True, engineio_logger=True, cors_allowed_origins="*")
 
-historical_data = {"Battery State of Charge": [], "Motor Temperature": []}
-
-
-@app.route("/")
-def index():
-    return "Flask server is running."
+historical_data = {
+    "Battery State of Charge": [],
+    "Motor Temperature": [],
+}
 
 
 def create_data():
-    motor_temp = DummySensorData("Motor Temperature", 60, 0, 100, "C")
-    battery_temp = DummySensorData("Battery Temperature", 47, 0, 60, "C")
-    motor_speed = DummySensorData("Motor Speed", 3654, 1500, 4700, "RPM")
-    battery_soc = DummySensorData("Battery State of Charge", 90, 0, 100, "%")
-    battery_voltage = DummySensorData("Battery Voltage", 286, 216, 300, "V")
-    battery_current = DummySensorData("Battery Current", 269, 0, 300, "A")
-    suspension_travel = DummySensorData("Suspension Travel", 48, 0, 78, "mm")
-    pedal_angle_1 = DummySensorData("Pedal Angle 1", 50, 0, 100, "%")
-    pedal_angle_2 = DummySensorData("Pedal Angle 2", 47, 0, 100, "%")
-    track_time = DummySensorData("Track Time", 0, 0, 1000, "s")
-    wheel_speed = DummySensorData("Wheel Speed", 2245, 0, 10000, "RPM")
-    warnings = DummySensorData("Warnings", 0, 0, 4, "System Faults")
-    break_pressure_front = DummySensorData(
-        "Break Pressure Front", 6000, -100, 8000, "kPa"
-    )
-    break_pressure_rear = DummySensorData(
-        "Break Pressure Rear", 5189, -100, 8000, "kPa"
-    )
-    high_voltage = DummySensorData("High Voltage", 0, 0, 1, "")
-    error = DummySensorData("Vehicle Errors", "Error", 0, 0, "")
-    predict_charge = DummySensorData("Predictive State of Charge", 45, 0, 100, "%")
-
     return [
-        motor_temp,
-        battery_temp,
-        motor_speed,
-        battery_soc,
-        battery_voltage,
-        battery_current,
-        suspension_travel,
-        pedal_angle_1,
-        pedal_angle_2,
-        track_time,
-        wheel_speed,
-        warnings,
-        break_pressure_front,
-        break_pressure_rear,
-        high_voltage,
-        error,
-        predict_charge,
+        DummySensorData("Motor Temperature", 60, 0, 100, "C"),
+        DummySensorData("Battery Temperature", 47, 0, 60, "C"),
+        DummySensorData("Motor Speed", 3654, 1500, 4700, "RPM"),
+        DummySensorData("Battery State of Charge", 90, 0, 100, "%"),
+        DummySensorData("Battery Voltage", 286, 216, 300, "V"),
+        DummySensorData("Battery Current", 269, 0, 300, "A"),
+        DummySensorData("Suspension Travel", 48, 0, 78, "mm"),
+        DummySensorData("Pedal Angle 1", 50, 0, 100, "%"),
+        DummySensorData("Pedal Angle 2", 47, 0, 100, "%"),
+        DummySensorData("Track Time", 0, 0, 1000, "s"),
+        DummySensorData("Wheel Speed LF", 2245, 0, 10000, "RPM"),
+        DummySensorData("Wheel Speed RF", 3654, 1500, 4700, "RPM"),
+        DummySensorData("Wheel Speed LB", 3654, 1500, 4700, "RPM"),
+        DummySensorData("Wheel Speed RB", 3654, 1500, 4700, "RPM"),
+        DummySensorData("Warnings", 0, 0, 4, "System Faults"),
+        DummySensorData("Break Pressure Front", 6000, -100, 8000, "kPa"),
+        DummySensorData("Break Pressure Rear", 5189, -100, 8000, "kPa"),
+        DummySensorData("High Voltage", 0, 0, 1, ""),
+        DummySensorData("Vehicle Errors", "Error", 0, 0, ""),
+        DummySensorData("Predictive State of Charge", 45, 0, 100, "%"),
     ]
 
 
@@ -71,22 +50,31 @@ def generate_data(sensors):
     sensors[8].update_value_step(step=7)  # pedal_angle_2
     sensors[9].update_value_increasing(step=1)  # track_time
     sensors[10].update_value_step(step=150)  # wheel_speed
-    sensors[11].update_fault_data()  # warnings
-    sensors[12].update_value_step(step=100)  # break_pressure_front
-    sensors[13].update_value_step(step=100)  # break_pressure_rear
-    sensors[14].update_value_boolean()  # high_voltage
-    sensors[15].update_string()  # error
-    sensors[16].update_value_step(step=3)  # predict_charge
+    sensors[11].update_value_step(step=150)  # wheel_speed1
+    sensors[12].update_value_step(step=150)  # wheel_speed2
+    sensors[13].update_value_step(step=150)  # wheel_speed3
+    sensors[14].update_fault_data()  # warnings
+    sensors[15].update_value_step(step=100)  # break_pressure_front
+    sensors[16].update_value_step(step=100)  # break_pressure_rear
+    sensors[17].update_value_boolean()  # high_voltage
+    sensors[18].update_string()  # error
+    sensors[19].update_value_step(step=3)  # predict_charge
 
     log_data(sensors)
 
 
 def log_data(sensors):
-    historical_data.get("Battery State of Charge").append(sensors[3].value)
+    timestamp = datetime.datetime.now().strftime("%s")
+    historical_data["Battery State of Charge"].append(
+        {"timestamp": timestamp, "value": sensors[3].value}
+    )
+    historical_data["Motor Temperature"].append(
+        {"timestamp": timestamp, "value": sensors[0].value}
+    )
 
 
 @socketio.on("testing")
-def handle_register():
+def handle_testing():
     sensors = create_data()
     while True:
         generate_data(sensors)
@@ -95,25 +83,24 @@ def handle_register():
 
 
 @socketio.on("history")
-def handle_register():
-    print("*********", historical_data)
+def handle_history():
     socketio.emit("historic_data", historical_data, to=request.sid)
 
 
 @socketio.on("update_clients")
-def handle_request():
-    socketio.emit("data", generate_data())
+def handle_update_clients():
+    sensors = create_data()
+    generate_data(sensors)
+    socketio.emit("data", [sensor.to_dict() for sensor in sensors])
 
 
 @socketio.on("connect")
 def handle_connect():
-    pass
     print(f"User connected!")
 
 
 @socketio.on("disconnect")
 def handle_disconnect():
-    pass
     print(f"User disconnected!")
 
 
