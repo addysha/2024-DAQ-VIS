dac0056a84d7f6fe16a90f3f621586c331b5824f
hm239 <hm239@students.waikato.ac.nz>
2024-10-08 16:50:46 +1300

    Fixing wrong min/max for motor speed

--- wesmo-app/src/components/dashboard/DefaultGrid.tsx
+++ wesmo-app/src/components/dashboard/DefaultGrid.tsx
@@ -291,8 +291,8 @@ const DefaultGrid: React.FC<Props> = ({ data, timer }) => {
             <BarContainer
               textValue={"Motor Speed"}
               currentValue={+(motorSpeed?.value ?? 0)}
-              maxValue={+(motorSpeed?.max ?? 0)}
-              minValue={+(motorSpeed?.min ?? 5200)}
+              maxValue={+(motorSpeed?.max ?? 5200)}
+              minValue={+(motorSpeed?.min ?? 0)}
               unit={motorSpeed?.unit ?? "RPM"}
               onError={handleError}
             />
