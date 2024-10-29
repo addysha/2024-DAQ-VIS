ddf72ff003b3ddc48458f49f37adac16c3a17f22
Hannah Murphy <murphyhk01@gmail.com>
2024-10-29 14:44:37 +1300

    modifcations

--- back_end/BMSTranslatorClass.py
+++ back_end/BMSTranslatorClass.py
@@ -78,7 +78,7 @@ class BMSTranslator:
             },
             {
                 "name": "Battery Voltage",
-                "value": decoded_message["Pack_Inst_Voltage"]/1000,
+                "value": decoded_message["Pack_Summed_Voltage"]/1000,
                 "unit": "V",
                 "max": 100,
             },
